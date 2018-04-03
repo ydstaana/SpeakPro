19290c45b0ecb43cd5ae47d36ace97d085ca15e5
Rain Tomista <rstomista@up.edu.ph>
2018-04-03 23:41:29 +0800

    Implement create user

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,17 +1,54 @@
+import { UserService } from './../../service/user.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../model/user';
+
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
+  providers:[UserService]
 })
 export class HomepageComponent implements OnInit {
   fullImagePath: string;
-  constructor() {
-  this.fullImagePath = '/assets/images'
-   }
+  studentForm: FormGroup;
+  teacherForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private userService: UserService) {
+
+    this.fullImagePath = 'assets/images';
+    this.studentForm = this.createForm('STUDENT');
+    this.teacherForm = this.createForm('TEACHER');
+  }
 
   ngOnInit() {
+
+  }
+
+  createForm(userType: String){
+    let form: FormGroup = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.required],
+      skypeID: ['', Validators.required],
+      userType: [userType],
+      newUser: [true]
+    });
+
+    return form;
+  }
+
+  submitForm(user: User){
+    this.userService.createUser(user)
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        err => console.log(err)
+      );
   }
 
 }
---
+++ src/model/user.ts
@@ -0,0 +1,10 @@
+export class User {
+  firstName: String;
+  lastName: String;
+  username: String;
+  password: String;
+  email: String;
+  skypeID: String;
+  userType: String;
+  newUser: Boolean;
+}
