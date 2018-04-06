06e67408026c3dd2b406b2d3dee60614b5bf05f2
Rain Tomista <rstomista@up.edu.ph>
2018-04-06 19:34:13 +0800

    Partial implementation of add classes

--- src/app/add-classes/add-classes.component.ts
+++ src/app/add-classes/add-classes.component.ts
@@ -1,40 +1,66 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import { Sched } from './../../model/sched';
 import { ClassService } from '../../service/class.service';
+import { FormBuilder, FormGroup, Validators, FormControl, FormArray } from '@angular/forms';
 
 
 @Component({
   selector: 'app-add-classes',
   templateUrl: './add-classes.component.html',
   styleUrls: ['./add-classes.component.css'],
-  providers:[ClassService]
+  providers: [ClassService]
 })
 export class AddClassesComponent implements OnInit {
-	/*TO DO
-	-api call to get list of available classes
-	-api call to add checked classes */
-  classes :Sched[];
+  classes: Sched[] = [];
+  form: FormGroup;
 
-  constructor(private classService: ClassService) { }
-
-  ngOnInit() {
-  	this.availableClasses();
+  constructor(private fb: FormBuilder, private classService: ClassService) {
+    this.createForm();
   }
 
-  availableClasses(){
+  ngOnInit() {
+    this.availableClasses();
+  }
+
+  availableClasses() {
     this.classService.getAvailableClasses()
-    .subscribe(
-        data => {
-          this.classes = data;
-          console.log(this.classes);
-        },
-        err => console.log(err)
+      .subscribe(
+      data => {
+        this.classes = data;
+        this.rebuildForm();
+      },
+      err => console.log(err)
       );
   }
 
-  clicked(event){
+  clicked(event) {
     console.log(event.currentTarget.value);
+  }
+
+  ngOnChanges(){
+    // this.rebuildForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      selectedClasses: this.fb.array([
+      ])
+    });
+  }
+
+  get selectedClasses(): FormArray {
+    return this.form.get('selectedClasses') as FormArray;
+  };
+
+  setClasses(classes: Sched[]){
+    const classesFGs = classes.map(el => this.fb.group(el));
+    const classesFormArray = this.fb.array(classesFGs);
+    this.form.setControl('selectedClasses', classesFormArray)
+  }
+
+  rebuildForm(){
+    this.setClasses(this.classes);
   }
 
 }
