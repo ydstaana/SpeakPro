7120a2172d4ba60fac15d7dbb6974e3141136cfd
ydstaana <ydstaana@up.edu.ph>
2018-04-13 23:08:59 +0800

    cors error no authorization header

--- server/routes/endpoints/admin/_auth/_v/_v1/login.js
+++ server/routes/endpoints/admin/_auth/_v/_v1/login.js
@@ -1,10 +1,33 @@
 var mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 var User = require('../../../../../../models/UserSchema.js');
+var secret = "speakpro";
 
 module.exports = function(req, res, next){
 	User.authenticate(req.body.username, req.body.password, function(err,user){
-		console.log(req.body)
 		if(err) return next(err);
-	    res.status(200).json(post);
+
+		else{
+			console.log(user);
+			const payload = {
+		        id : user._id,
+				firstName: user.firstName,
+				email: user.email,
+				lastName: user.lastName,
+				username: user.username,
+	        }
+
+	        var token = jwt.sign(payload, secret, {
+	          expiresIn : 3600 // expires in 1 hour
+	        });
+
+	        res.json({
+	        	id: user.id,
+	            success: true,
+	            message : "Token generated",
+	            token : token
+	         });
+
+		}
 	})
 }
---
+++ server/routes/endpoints/base_routes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+var secret = "speakpro"
+
+router.use(function(req, res, next) {
+//set headers to allow cross origin request.
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, My-Header");
+    next();
+});
+
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+/*JWT Routes Middleware*/
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.params.token || req.headers['x-access-token'] || req.headers['authorization'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, secret, function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+
+  }
+});
+
+module.exports = router;
