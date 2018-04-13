ff22d97639b907812ff817f9df993169f21efa9a
ydstaana <ydstaana@up.edu.ph>
2018-04-13 13:33:42 +0800

    fixed edit profile, view uploads

--- server/routes/endpoints/admin/_auth/_v/_v1/login.js
+++ server/routes/endpoints/admin/_auth/_v/_v1/login.js
@@ -5,7 +5,6 @@ module.exports = function(req, res, next){
 	User.authenticate(req.body.username, req.body.password, function(err,user){
 		console.log(req.body)
 		if(err) return next(err);
-		console.log(user);
-		res.json(user);
+	    res.status(200).json(post);
 	})
 }
--- server/routes/endpoints/admin/_user/_v/_v1/update_user.js
+++ server/routes/endpoints/admin/_user/_v/_v1/update_user.js
@@ -1,15 +1,34 @@
 var mongoose = require('mongoose');
 var User = require('../../../../../../models/UserSchema.js');
+var bcrypt = require('bcrypt');
 
 module.exports = function(req, res, next){
-	User.findOneAndUpdate({username: req.params.username}, req.body, function (err, post) {
-		if (err) res.status(500).json({
-	    	code : 500,
-	    	message : err
-	    });
-	    res.status(200).json({
-	    	code : 200,
-	    	message : "Successfully updated user"
-	    });
-	});
+	//Remove blank fields
+	for(item in req.body){
+		if(req.body[item] == null || req.body[item] == ""){
+			delete req.body[item];
+		}
+	}
+
+	//rehash password if password was changed
+	if(req.body["password"] != undefined){
+		bcrypt.hash(req.body["password"], 10, function (err, hash){
+	      if (err) {
+	        return next(err);
+	      }
+	      console.log(hash);
+	      req.body["password"] = hash;
+	      User.findOneAndUpdate({username: req.params.username}, req.body, function (err, post) {
+				if (err) res.status(500).json({
+			    	code : 500,
+			    	message : err
+			    });
+			    res.status(200).json({
+			    	code : 200,
+			    	message : "Successfully updated user"
+			    });
+			});
+	    })
+	}
+	
 }
