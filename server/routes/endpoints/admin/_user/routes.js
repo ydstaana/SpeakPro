ee2905d06ac81e7be1417a8e6d1bb1c981600659
Rain Tomista <rstomista@up.edu.ph>
2018-04-15 18:29:52 +0800

    Fix jwt auth issues in routes

--- server/routes/endpoints/admin/_user/routes.js
+++ server/routes/endpoints/admin/_user/routes.js
@@ -5,14 +5,16 @@ const get_single_user = require('./_v/_v1/get_single_user');
 const get_all_users = require('./_v/_v1/get_all_users');
 const create_user = require('./_v/_v1/create_user');
 const update_user = require('./_v/_v1/update_user');
+const core = require('../../services/core');
 
-router.get('/', function(req, res, next){
-	res.send(200, 'Api Works');
+
+router.get('/', function (req, res, next) {
+  res.send(200, 'Api Works');
 });
 
-router.get('/users', get_all_users);
-router.get('/users/:id', get_single_user);
+router.get('/users', core.verifyToken, get_all_users);
+router.get('/users/:id', core.verifyToken, get_single_user);
 router.post('/users', create_user);
-router.put('/users/:username', update_user);
+router.put('/users/:username', core.verifyToken, update_user);
 
 module.exports = router;
