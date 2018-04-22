9b155510dfb274fcef0ad5a0c3d17cdfc7d166ef
ydstaana <ydstaana@up.edu.ph>
2018-04-22 17:01:51 +0800

    added upload_files v2

--- server/models/FileSchema.js
+++ server/models/FileSchema.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose')
 
 var FileSchema = new mongoose.Schema({
 	fileName : String,
+	displayName:String,
 	author: {
 		type : mongoose.Schema.Types.ObjectId,
 		ref: 'User'
