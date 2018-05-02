cb794b86038ed65ee8c0a6d74addbc8acf9a9c4c
Rain Tomista <rstomista@up.edu.ph>
2018-05-03 03:04:20 +0800

    Remove closed class in student class codes and schedule

--- server/routes/endpoints/admin/_class/_v/_v1/close_class.js
+++ server/routes/endpoints/admin/_class/_v/_v1/close_class.js
@@ -3,9 +3,10 @@ var Schedule = require('../../../../../../models/SchedSchema.js');
 var User = require('../../../../../../models/UserSchema.js');
 
 module.exports = function (req, res, next) {
-  Schedule.findById(req.params.id)
-    .remove()
-    .exec(function (err, sched) {
+
+  //Messy code but it works
+  Schedule.findOneAndRemove({ _id: req.params.id })
+    .exec((err, sched) => {
       if (err) {
         res.status(500).json({
           code: 500,
@@ -14,14 +15,34 @@ module.exports = function (req, res, next) {
       }
       else {
         User.findById(req.body.teacher)
-          .exec(function (err, user) {
-            user.classCodes.pop(req.body.code);
-            user.save();
-            console.log(user);
-            res.status(200).json({
-              code: 200,
-              message: "Successfully closed class"
-            });
+          .exec((err, teacher) => {
+            if (teacher.classCodes) {
+              teacher.classCodes.pop(req.body.code);
+              teacher.save();
+            }
+
+
+            User.findById(req.body.student)
+              .exec((err, student) => {
+                if (student && student.classCodes && student.schedule) {
+                  student.classCodes.pop(req.body.code);
+                  student.schedule.pop(sched._id);
+                  student.save();
+                }
+
+                if (err) {
+                  res.status(500).json({
+                    code: 500,
+                    message: err
+                  });
+                }
+                else {
+                  res.status(200).json({
+                    code: 200,
+                    message: "Successfully closed class"
+                  });
+                }
+              });
           });
       }
     });
