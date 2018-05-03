220dfbdc90f4acd5708538db49a38dc135b4730a
Rain Tomista <rstomista@up.edu.ph>
2018-05-03 21:45:43 +0800

    Added the username of teacher of the class in get all available classes

--- server/routes/endpoints/admin/_class/_v/_v1/get_all_available_classes.js
+++ server/routes/endpoints/admin/_class/_v/_v1/get_all_available_classes.js
@@ -3,7 +3,7 @@ var Schedule = require('../../../../../../models/SchedSchema.js');
 
 module.exports = function (req, res, next) {
   Schedule.find({ available: true })
-    .populate('teacher', 'firstName lastName')
+    .populate('teacher', 'firstName lastName username')
     .sort({ code: 1 })
     .exec(function (err, schedules) {
       if (err) res.status(500).json({
