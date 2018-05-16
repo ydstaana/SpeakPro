18ec9a7f9250d228ab55fe36a4c350bc7b2d0da0
Rain Tomista <rstomista@up.edu.ph>
2018-05-16 17:03:57 +0800

    Fix login and dashboard auth

--- src/app/student.guard.ts
+++ src/app/student.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../service/auth.service';
 
 @Injectable()
 export class StudentGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private auth: AuthService) { }
 
   canActivate(): boolean {
-    const user = JSON.parse(localStorage.getItem('loggedUser'));
+    const user = this.auth.decodeAccessToken(localStorage.getItem('token'));
     if (user.userType === 'STUDENT') return true;
     this.router.navigate(['/dashboard/teacher/my-classes']);
     return false;
--- src/app/teacher.guard.ts
+++ src/app/teacher.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../service/auth.service';
 
 @Injectable()
 export class TeacherGuard implements CanActivate {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private auth: AuthService) { }
 
   canActivate(): boolean {
-    const user = JSON.parse(localStorage.getItem('loggedUser'));
+    const user = this.auth.decodeAccessToken(localStorage.getItem('token'));
     if (user.userType === 'TEACHER' || user.userType === 'ADMIN') return true;
     this.router.navigate(['/dashboard/my-schedule']);
     return false;
