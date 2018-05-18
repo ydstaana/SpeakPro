32a6df1f9aea736901a5e1bc3d587a0117b1f33a
Rain Tomista <rstomista@up.edu.ph>
2018-05-18 20:26:34 +0800

    Update forgot password endpoint

--- server/routes/endpoints/admin/_auth/_v/_v1/lost_password.js
+++ server/routes/endpoints/admin/_auth/_v/_v1/lost_password.js
@@ -27,66 +27,66 @@ smtpTransport.use('compile', hbs(handlebarsOptions));
 
 module.exports = function (req, res, next) {
 
-	// do functions one after another through async waterfall
+  // do functions one after another through async waterfall
   async.waterfall([
-  	function(done){
-  		console.log(req.body)
-  		User.findOne({
-			username: req.body.username
-  		}).exec(function(err, user){
-  			if(user){
-  				done(err, user);
-  			}
-  			else{
-  				done('User not found');
-  			}
-  		});
-  	},
-  	function(user,done){
-  		console.log(user);
-  		//generate random token
-  		bcrypt.hash(user.username, 10, function (err, hash){
-	      if (err) {
-	        return done(err);
-	      }
-	      var token = hash;
-	      done(err, user, token);
-	    })
-  	},
-  	function(user, token, done) {
-      User.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function(err, new_user) {
+    function (done) {
+      console.log(req.body)
+      User.findOne({
+        username: req.body.username
+      }).exec(function (err, user) {
+        if (user) {
+          done(err, user);
+        }
+        else {
+          done('The provided username does not exist. Please try again.');
+        }
+      });
+    },
+    function (user, done) {
+      console.log(user);
+      //generate random token
+      bcrypt.hash(user.username, 10, function (err, hash) {
+        if (err) {
+          return done(err);
+        }
+        var token = hash;
+        done(err, user, token);
+      })
+    },
+    function (user, token, done) {
+      User.findByIdAndUpdate({ _id: user._id }, { reset_password_token: token, reset_password_expires: Date.now() + 86400000 }, { upsert: true, new: true }).exec(function (err, new_user) {
         done(err, token, new_user);
       });
     },
-  	function(token, user, done){
-  		console.log(token);
-  		console.log(user);
-  		var data = {
-  			to: user.email,
-  			from : email,
-  			template: 'forgot-password-email',
-  			subject: 'Lost password',
-  			context: {
-  				url: 'http://localhost:4200/recovery&'+token,
-  				name: user.firstName,
-  				token: token
-  			}
-  		};
+    function (token, user, done) {
+      console.log(token);
+      console.log(user);
+      var data = {
+        to: user.email,
+        from: email,
+        template: 'forgot-password-email',
+        subject: 'Lost password',
+        context: {
+          url: 'http://localhost:4200/#/reset?token=' + token,
+          name: user.firstName,
+          token: token
+        }
+      };
 
-  	smtpTransport.sendMail(data, function(err){
-  		if(!err){
-  			return res.json({
-  				message: 'Kindly check your email for further instructions'
-  			})
-  		}
-  		else{
-  			return done(err);
-  		}
-  	})
-  	}
-  	], function(err){
-  		return res.status(422).json({
-  			message: err
-  		});
-  	});
+      smtpTransport.sendMail(data, function (err) {
+        if (!err) {
+          return res.json({
+            message: 'Kindly check your email for further instructions'
+          })
+        }
+        else {
+          return done(err);
+        }
+      })
+    }
+  ], function (err) {
+    return res.status(422).json({
+      message: err
+    });
+  });
 }
