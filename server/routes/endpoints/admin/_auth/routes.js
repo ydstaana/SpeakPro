84514d1f9263bc50d77172248b2cd9f4b60dd4c2
Rain Tomista <rstomista@up.edu.ph>
2018-05-23 03:28:22 +0800

    Implement confirm password endpoint

--- server/routes/endpoints/admin/_auth/routes.js
+++ server/routes/endpoints/admin/_auth/routes.js
@@ -5,9 +5,11 @@ const router = express.Router();
 const login = require('./_v/_v1/login');
 const lost_password = require('./_v/_v1/lost_password');
 const reset_password = require('./_v/_v1/reset_password');
+const confirm_password = require('./_v/_v1/confirm_password');
 
 router.post('/users/login', login);
 router.post('/users/lost_password', lost_password);
 router.post('/users/reset_password', reset_password);
+router.post('/users/confirm_password', confirm_password);
 
 module.exports = router;
