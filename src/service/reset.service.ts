1214d1985caad16e00c32672a5122b651c26d2d3
Rain Tomista <rstomista@up.edu.ph>
2018-05-30 01:03:30 +0800

    Fix issues in forgot and reset password

--- src/service/reset.service.ts
+++ src/service/reset.service.ts
@@ -5,16 +5,11 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class ResetService {
   constructor(private http: HttpClient) { }
 
-  forgotPassword(username) {
-    return this.http.post(`http://localhost:3000/users/lost_password`,
-      { username: username }, { headers: this.getHeaders() });
-  }
-  resetPassword(token, password) {
-    return this.http.post(`http://localhost:3000/users/reset_password`,
-      { token: token, password: password }, { headers: this.getHeaders() });
+  forgotPassword(data) {
+    return this.http.post('http://localhost:3000/users/lost_password', data).pipe();
   }
 
-  getHeaders() {
-    return new HttpHeaders({ 'Authorization': localStorage.getItem('token') });
+  resetPassword(token, password) {
+    return this.http.post(`http://localhost:3000/users/reset_password`, { token: token, password: password }).pipe();
   }
 }
