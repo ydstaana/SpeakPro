05637b8b61e8799a195ffda41c17131de6cf2497
Rain Tomista <rstomista@up.edu.ph>
2018-05-29 15:59:49 +0800

    Implement add to cart

--- server/routes/endpoints/admin/_checkout/routes.js
+++ server/routes/endpoints/admin/_checkout/routes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const core = require('../../services/core');
 
+const add_to_cart = require('./_v/_v1/add_to_cart');
 const checkout = require('./_v/_v1/checkout');
 
+
+router.post('/cart/add-items', core.verifyToken, add_to_cart);
 router.post('/checkout', core.verifyToken, checkout);
 
 module.exports = router;
